04bb9485381465bc5d07143e40b2fc75382a25d4
MasaCode <masacode.vancouver@gmail.com>
2019-12-06 16:02:27 +0900

    :+1: Add Model Validation for system master tables

--- app/models/deploy_method.rb
+++ app/models/deploy_method.rb
@@ -1,3 +1,6 @@
 class DeployMethod < ApplicationRecord
   self.primary_key = :deploy_method_id
+
+  validates :deploy_method_id, presence: true, uniqueness: true, numericality: { only_integer: true }
+  validates :deploy_method_name, presence: true
 end
