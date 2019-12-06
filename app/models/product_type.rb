4b8b4e01e2de606fafc719e04c8fa771fc0c9a9f
MasaCode <masacode.vancouver@gmail.com>
2019-12-06 11:43:35 +0900

    :white_check_mark:  Add RSpec test for ProductType model

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,6 +1,6 @@
 class ProductType < ActiveRecord::Base
-  self.primary_key = :product_id
+  self.primary_key = :product_type_id
 
-  validates :product_id, presence: true, uniqueness: true, numericality: { only_integer: true }
+  validates :product_type_id, presence: true, uniqueness: true, numericality: { only_integer: true }
   validates :product_type_name, presence: true
 end
