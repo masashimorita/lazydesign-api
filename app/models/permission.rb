d1b82a33919c128d1de55bb0c67598f73402a3f5
MasaCode <masacode.vancouver@gmail.com>
2019-12-15 19:06:55 +0900

    :sparkles: Defined callback method to check if the master record is deletable & dependent options for association

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,9 +1,9 @@
 class Permission < ApplicationRecord
   self.primary_key = :permission_id
 
-  has_many :users_permissions, primary_key: :user_permission_id, foreign_key: :user_permission_id
+  has_many :users_permissions, primary_key: :user_permission_id, foreign_key: :user_permission_id, dependent: :destroy
   has_many :users, through: :users_permissions, source: :user
-  has_many :plans_permissions, primary_key: :plan_id, foreign_key: :plan_id
+  has_many :plans_permissions, primary_key: :plan_id, foreign_key: :plan_id, dependent: :destroy
   has_many :plans, through: :plans_permissions, source: :plan
 
   validates :permission_id, presence: true, uniqueness: { case_sensitive: true }, numericality: { only_integer: true }
